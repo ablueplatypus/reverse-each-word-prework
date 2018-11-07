4236f45a63ebd8965ccfb0c933109f77e70b4dea
Matt Doering <mattdoering86@gmail.com>
2018-11-07 16:01:47 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,12 @@
-# def reverse_each_word(sentence)
-#   arr = sentence.split
-#   new_arr = []
-#   arr.each do |el|
-#     el.reverse
-#     new_arr.push(el)
-#   end
-#   new_arr.join(" ")
-# end
+def reverse_each_word(sentence)
+  arr = sentence.split
+  new_arr = []
+  arr.each do |el|
+    el.reverse
+    new_arr.push(el)
+  end
+  new_arr.join(" ")
+end
 
 def reverse_each_word(sentence)
   array_of_words = sentence.split(" ")
